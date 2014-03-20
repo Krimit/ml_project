ea037d2bcbe0c9747652e80ec65376c3247a9fa2
Asher Krim <asher.krim@nyu.edu>
2014-03-20 17:42:19 -0400

    initial ml project version

---
+++ api.py
@@ -0,0 +1,32 @@
+#kitties!
+#from urllib2 import urlopen
+
+#website = urlopen("http://placekitten.com/")
+#kittens = website.read()
+
+
+#print kittens[559:1000]	
+
+############################
+
+#api stuff
+#import requests
+#import pprint
+
+#query_params = { 'apikey': 'f1f2058236884fc4bfb1baabe60e6922',
+#				 'per_page': 3,
+# 		   		 'phrase': 'election night',
+# 		   		 'state':'MD'
+# 		 }
+
+# endpoint = 'http://capitolwords.org/api/text.json'
+# response = requests.get(endpoint, params= query_params)
+
+# data = response.json()
+# pprint.pprint(data)
+
+
+########################
+api_key = 'v0N7wNo4EROVSLXOUO254n6tc3Ia'
+consumer_key = 'KXG5H7YKajlO2V6anrqL9Z8nmVUa'
+consumer_secret = 'u5M_ViUm_7eicv3vSVHNWKwvPLka'
