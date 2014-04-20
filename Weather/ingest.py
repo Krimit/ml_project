f217bab7e2faf7ae65220d8f2fa146a5669d69f6
Asher Krim <asher.krim@nyu.edu>
2014-04-20 14:53:59 -0400

    feature building simple

--- Weather/ingest.py
+++ Weather/ingest.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 import csv
+import string
 
 np.set_printoptions(threshold=np.nan)
 
@@ -48,10 +49,28 @@ k13=25
 k14=26
 k15=27
 
+_positive_words = []
+_negative_words = []
+
+
 #
 # This function reads in the data file
 # and aggregates it in some to-be-determined fashion.
 def readData(filename):
+
+	global _negative_words
+	global _positive_words
+
+	#open sentiment files and store in global lists
+	f = 'negative.txt'
+	with open(f, 'r') as d:
+		_negative_words = d.read()
+
+	f = 'positive.txt'
+	with open(f, 'r') as d:
+		_positive_words = d.read()	
+
+	
 
 	tweets = []
 	locations = []
@@ -68,7 +87,7 @@ def readData(filename):
 	# print np_array.shape
 	# print data.shape
 	# print labels.shape
-	return data
+	return data, labels
 
 #
 # build the corpus given an array with words. A
@@ -79,28 +98,46 @@ def readData(filename):
 #
 # Note that location data is included in corpus.
 #
-def buildCorpus(data, cutoff=5):
+def buildCorpus(data, cutoff=30):
+
+
+
 	print 'building corpus with threshold = ', cutoff
 
 	corpus = {}
-	orig_data = []
-
+	tweets = []
+	locations = []
+	locations1 = []
+	locations2 = []
 	
 	for line in data:
-		for d in line:
-			if type(d) is float and math.isnan(d):
-				d = ''
-				
-			l = d.split()
-			x = set()
-			for word in l:
-				x.add(word)
-			for word in x:	
-				if word in corpus:
-					corpus[word] += 1
-				else:
-					corpus[word] = 1	
-			orig_data.append(x)
+		#print line[0]
+		#print line[1]
+		#print line[2]
+
+
+		#NOTE: currently location data NOT used in building corpus
+		loc1 = cleanNans(line[1])
+		locations1.append(loc1)
+
+		loc2 = cleanNans(line[2])
+		locations2.append(loc2)		
+
+		l = cleanNans(line[0]).split()
+		x = list()
+		for word in l:
+			word = word.lower()
+			to_token=filter(is_not_punct,word)
+			#print word , ' ', to_token
+			if not to_token in x:
+				x.append(to_token)
+			
+		for word in x:	
+			if word in corpus:
+				corpus[word] += 1
+			else:
+				corpus[word] = 1	
+		tweets.append(x)
 
 
 	# remove words without enough occurrences
@@ -109,34 +146,64 @@ def buildCorpus(data, cutoff=5):
 		if corpus[word] < cutoff:
 			del updated_corpus[word]
 	corpus = updated_corpus
+	corpus = corpus.keys()
 
-	return corpus, orig_data		
+	return corpus, tweets, zip(locations1, locations2)		
 
+def cleanNans(d):
+	if type(d) is float and math.isnan(d):
+		return ''
+	return d	
+
+# Function to use for filtering
+def is_not_punct(char):
+	# keep dashes, all other punctuation gets removed
+	if char == '-': return True
+	elif char in string.punctuation: return False
+	else: return True
+
+#
+#
+#
 def buildFeatureVectors(corpus, orig_data):
+	global _negative_words
+	global _positive_words
+
 	#build feature vectors
 	length = len(corpus)
 	height = len(orig_data)
+
 	
 	row = 0
 	data = np.zeros( (height,length) )
+	sentiment = np.zeros( (height,2) )
+	positive_counter = 0
+	negative_counter = 0
 	for line in orig_data:
-		
+		positive_counter = 0
+		negative_counter = 0
 		col = 0
-		for word in corpus:
-			if word in line:
-				data[row][col] = 1
-			else:
-				data[row][col] = 0
+		for word in line:
+			if word in corpus:
+				data[row][corpus.index(word)] = 1
+			if word in _negative_words:
+				negative_counter += 1
+			if word in _positive_words:
+				positive_counter += 1		
 			col += 1	
+		sentiment[row][0] = positive_counter/len(line)
+		sentiment[row][1] = negative_counter/len(line)
 		row += 1
 		print row
 
-	return data			
+	#print sentiment
+	return data, sentiment			
 
 
 if __name__ == "__main__":
     import sys
-    data = readData(str(sys.argv[1]))
-    corpus, orig_data = buildCorpus(data)
-    data = buildFeatureVectors(corpus, orig_data)
-    print data
+    data, labels = readData(str(sys.argv[1]))
+    corpus, orig_data, locations = buildCorpus(data)
+    
+    data, sentiment = buildFeatureVectors(corpus, orig_data)
+
